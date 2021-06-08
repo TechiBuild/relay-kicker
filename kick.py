1db10d7bf7c58a39e043d77b4e0fee71bdac1ee6
Dale <TechiBuild@users.noreply.github.com>
2021-06-08 14:22:21 +0100

    Update kick.py

--- kick.py
+++ kick.py
@@ -3,13 +3,16 @@ import time
 GPIO.setmode(GPIO.BCM) # GPIO Numbers instead of board numbers
  
 relay_1 = 17
-relay_2 = 18
+relay_2 = 27
 
 GPIO.setup(relay_1, GPIO.OUT) # GPIO Assign mode
 GPIO.setup(relay_2, GPIO.OUT) # GPIO Assign mode
 
-GPIO.output(relay_GPIO_PIN, GPIO.LOW) # out
-GPIO.output(relay_GPIO_PIN, GPIO.LOW) # out
+GPIO.output(relay_1, GPIO.LOW) # out
+GPIO.output(relay_2, GPIO.LOW) # out
 time.sleep(5)
-GPIO.output(relay_GPIO_PIN, GPIO.HIGH) # on
-GPIO.output(relay_GPIO_PIN, GPIO.HIGH) # on
+GPIO.output(relay_1, GPIO.HIGH) # on
+GPIO.output(relay_2, GPIO.HIGH) # on
+time.sleep(5)
+GPIO.output(relay_1, GPIO.LOW) # out
+GPIO.output(relay_2, GPIO.LOW) # out
