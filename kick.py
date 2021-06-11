db0e26bef8e6d24e1fca332c1b210240b2e03d08
Dale <TechiBuild@users.noreply.github.com>
2021-06-11 14:30:15 +0100

    Update kick.py

--- kick.py
+++ kick.py
@@ -1,18 +1,23 @@
 import RPi.GPIO as GPIO
 import time
-GPIO.setmode(GPIO.BCM) # GPIO Numbers instead of board numbers
- 
-relay_1 = 17
-relay_2 = 27
+
+GPIO.cleanup()
+
+GPIO.setmode(GPIO.BOARD) # GPIO Numbers instead of board numbers
+
+relay_1 = 32
+relay_2 = 36
 
 GPIO.setup(relay_1, GPIO.OUT) # GPIO Assign mode
 GPIO.setup(relay_2, GPIO.OUT) # GPIO Assign mode
 
-GPIO.output(relay_1, GPIO.LOW) # out
-GPIO.output(relay_2, GPIO.LOW) # out
-time.sleep(5)
-GPIO.output(relay_1, GPIO.HIGH) # on
-GPIO.output(relay_2, GPIO.HIGH) # on
-time.sleep(5)
-GPIO.output(relay_1, GPIO.LOW) # out
-GPIO.output(relay_2, GPIO.LOW) # out
+while True:
+
+  # Button Off
+  GPIO.output(relay_1, GPIO.HIGH)
+  GPIO.output(relay_2, GPIO.HIGH)
+  time.sleep(10)
+  # Button On
+  GPIO.output(relay_1, GPIO.LOW)
+  GPIO.output(relay_2, GPIO.LOW)
+  time.sleep(1)
